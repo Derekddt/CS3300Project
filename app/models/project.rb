7ba97d393fdef80d830bc2cc578cc137e6b3d549
Derekddt <derekddt@gmail.com>
2022-11-15 00:13:05 +0000

    Testing for circleCI failures by commenting out working code

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+    #validates :title, presence: true
+    #validates :description, presence: true
     #validates_presence_of :title
     #validates_presence_of :description
 end
